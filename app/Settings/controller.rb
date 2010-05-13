167184d633b9d49ebc081db0afa794d4a96de77d
lars <larsburgess@gmail.com>
2010-05-13 15:48:09 -0700

    initial commit

---
+++ app/Settings/controller.rb
@@ -0,0 +1,92 @@
+require 'rho'
+require 'rho/rhocontroller'
+require 'rho/rhoerror'
+require 'helpers/browser_helper'
+
+class SettingsController < Rho::RhoController
+  include BrowserHelper
+  
+  def index
+    @msg = @params['msg']
+    render
+  end
+
+  def login
+    @msg = @params['msg']
+    render :action => :login, :back => '/app'
+  end
+
+  def login_callback
+    errCode = @params['error_code'].to_i
+    if errCode == 0
+      # run sync if we were successful
+      WebView.navigate Rho::RhoConfig.options_path
+      SyncEngine.dosync
+    else
+      if errCode == Rho::RhoError::ERR_CUSTOMSYNCSERVER
+        @msg = @params['error_message']
+      end
+        
+      if !@msg || @msg.length == 0   
+        @msg = Rho::RhoError.new(errCode).message
+      end
+      
+      WebView.navigate ( url_for :action => :login, :query => {:msg => @msg} )
+    end  
+  end
+
+  def do_login
+    if @params['login'] and @params['password']
+      begin
+        SyncEngine.login(@params['login'], @params['password'], (url_for :action => :login_callback) )
+        AppApplication.start_bulk_sync = Time.now
+        
+        render :action => :wait
+      rescue Rho::RhoError => e
+        @msg = e.message
+        render :action => :login
+      end
+    else
+      @msg = Rho::RhoError.err_message(Rho::RhoError::ERR_UNATHORIZED) unless @msg && @msg.length > 0
+      render :action => :login
+    end
+  end
+  
+  def logout
+    SyncEngine.logout
+    @msg = "You have been logged out."
+    render :action => :login
+  end
+  
+  def reset
+    render :action => :reset
+  end
+  
+  def do_reset
+    Rhom::Rhom.database_full_reset
+    SyncEngine.dosync
+    @msg = "Database has been reset."
+    redirect :action => :index, :query => {:msg => @msg}
+  end
+  
+  def do_sync
+    SyncEngine.dosync
+    @msg =  "Sync has been triggered."
+    redirect :action => :index, :query => {:msg => @msg}
+  end
+  
+  def bulk_callback
+    puts "bulk_callback: #{@params}"
+
+    if @params['bulk_status'] == 'start' && @params['partition'].length == 0
+        AppApplication.start_bulk_sync = Time.now
+        puts "Start time = " + AppApplication.start_bulk_sync.to_i.inspect
+        WebView.navigate (url_for :action => :wait)
+    end    
+  
+    AppApplication.end_bulk_sync = Time.now
+    AppApplication.bulk_sync_total_time = 
+      AppApplication.end_bulk_sync.to_i - AppApplication.start_bulk_sync.to_i
+    WebView.navigate Rho::RhoConfig.start_path unless @params['status'] == 'in_progress'
+  end
+end
---
+++ app/application.rb
@@ -0,0 +1,20 @@
+require 'rho/rhoapplication'
+
+class AppApplication < Rho::RhoApplication
+  class << self
+    attr_accessor :start_bulk_sync
+    attr_accessor :end_bulk_sync
+    attr_accessor :bulk_sync_total_time
+  end
+  
+  def initialize
+    # Tab items are loaded left->right, @tabs[0] is leftmost tab in the tab-bar
+    @tabs = nil
+
+    @@tabbar = nil
+    # Important to call super _after_ you define @tabs!
+    
+    SyncEngine::set_bulk_notification("/app/Settings/bulk_callback", "")
+    super
+  end
+end
